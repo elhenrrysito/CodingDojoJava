550e698db9bb40b6ad7146806237c40b29d4fc4c
elhenrrysito <henrrydanielmejia@gmail.com>
2021-05-26 14:25:26 -0400

    mi primer programa

---
+++ Mi primer programa/MiPrimerPrograma.java
@@ -0,0 +1,11 @@
+/**
+ * MiPrimerPrograma
+ */
+public class MiPrimerPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Henrry Mejia");
+        System.out.println("Tengo 20 años de edad.");
+        System.out.println("Mi ciudad es Santiago de Chile, Chile");
+    }
+}
